a1092a1fcb9ce7b4adcfbe9297945d0318693963
Raul <rsanchez7@eogresources.com>
2021-05-18 16:29:49 -0500

    stopping point

--- html_only/app.js
+++ html_only/app.js
@@ -1,7 +1,25 @@
-const app = Vue.createApp({
-    data(){
-        return { courseGoal: 'Does this work??' }
+const app = new Vue({
+    el: '#user-goal',
+    data() {
+        //a function that returns an object
+        return {
+            courseGoal: 'biiiiihhhhh',
+            vueLink: "https://vuejs.org/",
+            courseGoalA: "Learn Vue",
+            courseGoalB: "Master Vue",
+        }
+    },
+    methods: {
+        //an object full of methods
+        outputGoal(){
+            const randomNumber = Math.random()
+            if(randomNumber < 0.5){
+                return this.courseGoalA
+            }
+            else{
+                return this.courseGoalB
+            }
+        }
     }
 });
 
-app.mount('#user-goal');
